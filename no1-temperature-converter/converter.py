30f2fb4fd78f0b6033f2b992e8ed016bb29fabc6
Javier <thesoap0jb@gmail.com>
2026-02-06 09:58:22 -0500

    fix: used except for better validation

--- no1-temperature-converter/converter.py
+++ no1-temperature-converter/converter.py
@@ -10,23 +10,24 @@ while True :
         
     elif scale == "c": 
         while True :
-            fahrenheit = input("Now write the temperature you want to convert in Fahrenheit degrees: \n")
-            if fahrenheit.isalpha():
-                print("Please try again.")
-            else: 
+            try:
+                fahrenheit = input("Now write the temperature you want to convert in Fahrenheit degrees: \n")
                 result_ftoc = (float(fahrenheit) - 32)*(5/9)
                 print(f"The temperature you inserted in Fahrenheit degrees: {fahrenheit}. \nConvertion to Celsius degrees: {result_ftoc}.")
                 break
+            except ValueError:
+                print("Please try again.")
+    
 
     elif scale == "f": 
         while True:
-            celsius = input("Now write the temperature you want to convert in Celsius degrees: \n")
-            if celsius.isalpha():
-                print("Please try again.")
-            else: 
+            try:
+                celsius = input("Now write the temperature you want to convert in Celsius degrees: \n")
                 result_ctof = (float(celsius) * 9/5)+32
                 print(f"The temperature you inserted in Celsius degrees: {celsius}. \nConvertion to Fahrenheit degrees: {result_ctof}.")
                 break
+            except ValueError:
+                print("Please try again.")
 
     elif scale == "x":
         print ("Thank you for using this program.")
