700dc095cfc59fe67dfe782fe7bc012840e4b123
Javier <thesoap0jb@gmail.com>
2026-02-06 09:50:16 -0500

    refactor: now .lower is used only once

--- no1-temperature-converter/converter.py
+++ no1-temperature-converter/converter.py
@@ -3,11 +3,12 @@ print("Welcome to the temperature converter.")
 while True :
 
     scale = input("Please, write down what temperature scale you want to convert to (c for celsius/f for fahrenheit/x to exit): \n")
+    scale = scale.lower()
 
     if len(scale) != 1 or scale.lower() not in ["c", "f", "x"]:
         print ("Please try again.")
         
-    elif scale.lower() == "c": 
+    elif scale == "c": 
         while True :
             fahrenheit = input("Now write the temperature you want to convert in Fahrenheit degrees: \n")
             if fahrenheit.isalpha():
@@ -17,7 +18,7 @@ while True :
                 print(f"The temperature you inserted in Fahrenheit degrees: {fahrenheit}. \nConvertion to Celsius degrees: {result_ftoc}.")
                 break
 
-    elif scale.lower() == "f": 
+    elif scale == "f": 
         while True:
             celsius = input("Now write the temperature you want to convert in Celsius degrees: \n")
             if celsius.isalpha():
@@ -27,6 +28,6 @@ while True :
                 print(f"The temperature you inserted in Celsius degrees: {celsius}. \nConvertion to Fahrenheit degrees: {result_ctof}.")
                 break
 
-    elif scale.lower() == "x":
+    elif scale == "x":
         print ("Thank you for using this program.")
         break
